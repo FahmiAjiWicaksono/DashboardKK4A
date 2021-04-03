c5fe297889c073f933fd3adfac6b8b31cf6eda96
FahmiAjiWicaksono <3103119066@student.smktelkom-pwt.sch.id>
2021-04-03 17:34:40 +0700

    KK4ADashboard

---
+++ Dashboard/app/src/main/java/com/FahmiAjiWicaksono/KK4ADashboard/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.FahmiAjiWicaksono.KK4ADashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
